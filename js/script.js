64914c89667030c96168fd2ba13972010f9fe01c
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-03-12 01:03:25 +0000

    Added JavaScript calculations

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2022 Maria Goemans All rights reserved
+// Created by: Maria Goemans
+// Created on: March 11 2022
+// This file contains the JS functions for index.html
+
+function CalculateClicked() {
+  // this function calculates the area and perimeter of the rectangle
+  document.getElementById('area').innerHTML = 'The area is ' + (7 * 4)
+  document.getElementById('perimeter').innerHTML = 'The perimeter is ' + (7 + 7 + 4 + 4)
+}
